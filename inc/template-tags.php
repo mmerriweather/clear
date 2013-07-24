ff787fa0594b82a5036e05c72ce6a6e5617a527a
Michelle Merriweather <mmerriweather12@gmail.com>
2013-07-24 03:04:55 -0400

    Initial Commit

---
+++ inc/template-tags.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Custom template tags for this theme.
+ *
+ * Eventually, some of the functionality here could be replaced by core features
+ *
+ * @package Clear
+ * @since Clear 1.0
+ */
