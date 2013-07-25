3a7927c023de970f1779bde8f973937c25793c5d
Michelle Merriweather <mmerriweather12@gmail.com>
2013-07-25 00:53:08 -0400

    Edited index.php added new functions uploaded to wordpress

--- header.php
+++ header.php
@@ -51,7 +51,11 @@ echo ' | ' . sprintf( __( 'Page %s', 'clear' ), max( $paged, $page ) );
 
 <div id="page" class="hfeed site">
      <header id="masthead" class="site-header" role="banner">
-         <hgroup></hgroup>
-         <nav role="navigation" class="site-navigation main-navigation"></nav><!-- .site-navigation .main-navigation -->
+         <hgroup><hgroup>
+     <h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+     <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+</hgroup></hgroup>
+         <nav role="navigation" class="site-navigation main-navigation"><h1 class="assistive-text"><?php _e( 'Menu', 'shape' ); ?></h1>
+<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_s' ); ?>"><?php _e( 'Skip to content', 'clear' ); ?></a></div><?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?></nav><!-- .site-navigation .main-navigation -->
      </header><!-- #masthead .site-header -->
 <div id="main" class="site-main">
